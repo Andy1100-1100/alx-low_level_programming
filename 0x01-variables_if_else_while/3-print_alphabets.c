f407ee22c1d37ec85b8ab00499d2579dbc4431f8
Andy1100-1100 <andyansah11@gmail.com>
2023-09-09 15:17:51 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,19 @@
 /**
  * main - prints lowercase and uppercase alphabet
  *
- * Renturn: Always 0
+ * Return: Always 0
  */
 int main(void)
 
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
-	putar('\n');
+
+	putchar('\n');
 
 	return (0);
-	/* Return 0 to indicate success*/
 }
